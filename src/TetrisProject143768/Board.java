be646260ac309afbb5363c2f1e7e23a6df20a139
Isaiah Lee <ShaeoSindee@gmail.com>
2019-01-17 21:27:38 -0600

    depracated block from board & main

--- src/TetrisProject143768/Board.java
+++ src/TetrisProject143768/Board.java
@@ -3,7 +3,7 @@ package TetrisProject143768;
 public class Board {
 	// board vars
 	// board declared but not initialized
-	private Block[][] board;
+	private char[][] board;
 	// Static constant vars so we only need to change them in one place, right here
 	// also so we don't have to do a array.length check which takes up valuable
 	// processor time.
@@ -13,14 +13,13 @@ public class Board {
 	// constructor
 	public Board() {
 		// init board
-		board = new Block[BOARDHEIGHT][BOARDWIDTH];
+		board = new char[BOARDHEIGHT][BOARDWIDTH];
 		// initialize array
 		for (int y = 0; y < BOARDWIDTH; y++) {
 			for (int x = 0; x < BOARDHEIGHT; x++) {
-				board[x][y] = new Block('e');
+				board[x][y] = 'e';
 			}
 		}
-		// getRandomboard();
 	}
 
 	// getters and setters
